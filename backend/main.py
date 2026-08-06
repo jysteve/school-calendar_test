52961c800ed0ac85c87f34cd69018ce038e5f7d9
jysteve0318-hue <jysteve_10@naver.com>
2026-08-06 22:19:36 +0900

    second

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # backend/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -28,35 +27,3 @@ app.include_router(assessment_router)
 @app.get("/")
 def root():
     return {"message": "Server is running"}
-=======
-# backend/main.py
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from backend.routes.assessment import router as assessment_router
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-# CORS 설정: 프런트엔드(로컬)에서도 호출 가능하도록 Origin 허용
-origins = [
-    "http://127.0.0.1:8001",
-    "http://localhost:8001",
-    "http://localhost:8001/",
-    "https://school-calendar-test.vercel.app"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-# 라우터 등록
-app.include_router(assessment_router)
-
-# 기본 라우트(테스트용)
-@app.get("/")
-def root():
-    return {"message": "Server is running"}
->>>>>>> 0fa52f1 (a)
